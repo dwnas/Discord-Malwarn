6859650d78cb6e9a242cc23f59763f7734966995
dwnas <lew@null.net>
2024-12-31 18:11:41 +0000

    Update README, Include required files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Discord-Malwarn
+# Discord Malwarn
+A simple project that will disallow you from interacting with malicious-marked links 
+
+## Usage
+### Requirements
+Either one of:
+* Vencord, Betterdiscord, or another clientmod that allows for custom css and/or themeing
+* Stylus, or another extension that allows for custom css on webpages
+---
+Then, simply insert this CSS wherever appropriate:
+```css
+@import(url("https://raw.githubusercontent.com/dwnas/Discord-Malwarn/refs/heads/main/import.css"))
+```
+
---
+++ generate.py
@@ -0,0 +1,22 @@
+def generate_css() -> list:
+    with open("malicious links", "r") as f:
+        links = f.read().split("\n")
+    
+    for link in links:
+        links[links.index(link)] = f"[href^='{link}']"
+    
+    return links
+
+def write_import_file() -> None:
+    with open("template.min.css", "r") as f:
+        template = f.read()
+
+    links = generate_css()
+
+    with open("import.css", "w") as f:
+        f.write(template.replace("$$links$$", ",".join(links)))
+
+# 
+
+write_import_file()
+    
